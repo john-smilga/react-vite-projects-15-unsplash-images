3b1456c19b4c7bc25994c328a1a981246a6240e2
john smilga <johnsmilga@gmail.com>
2023-04-04 09:48:52 -0400

    add react query dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AppProvider } from './context';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
